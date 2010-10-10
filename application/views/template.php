fea7340a1a8b72dbc5c0bd1a6eaa8f75351f13cb
Leonti Bielski <prishelec@gmail.com>
2010-10-10 18:46:20 +0200

    Google analytics added

--- application/views/template.php
+++ application/views/template.php
@@ -124,6 +124,20 @@ $content->multiplicator = $multiplicator;
         <a href = "http://fitmaestro.com/Privacy.html">Privacy Policy</a>
     </div>
 </div>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-19022201-1']);
+  _gaq.push(['_setDomainName', '.fitmaestro.com']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 </body>
 </html>
  
