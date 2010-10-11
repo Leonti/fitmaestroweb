b3371b885aed4f368d14794068dcb0e0f85d0c77
Leonti Bielski <prishelec@gmail.com>
2010-10-11 02:20:33 +0200

    Added short tutorial for home page

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,3 +1,37 @@
-
-<h2>Home page</h2>
-<h3>Probably some stats</h3>
+<div class="details-container">
+    <p>
+        Welcome to FitMaestro!<br />
+        Here is a short description of what is available for you:
+    </p>
+    <p>
+        <span class="emp">Exercises</span> - list of exercises divided into groups.<br />
+        You can import exercises from public exercise or create your own exercises.<br />
+        If you think your exercise can be useful for other users you can publish it.
+    </p>
+    <p>
+        <span class="emp">Programs</span> are your long-term exercising plans.<br />
+        You can decide what exercises at what days you want to do and then just follow the plan to achieve your goals!
+        <span class="emp">Public Programs</span> is a list of programs already designed for you to follow.
+        Every program has description so you decide if it's worth importing.<br />
+        As with exercises you can publish your program if you think it will be useful to other users.
+    </p>
+    <p>
+        <span class="emp">Workouts</span> are sets of exercises/workout routines. <br />
+        Workouts are parts of exercising programs but can be used separately. <br />
+        For example you can create workout for Monday and use it every week for exercising.
+    </p>
+    <p>
+        <span class="emp">Sessions</span> - this where you store your actual exercising results - repetitions and weights.
+        It can be based on plans you made in program or workout routine.<br />
+        You can also add exercises to session as you go - when you feel the need to do more then you planned.
+    </p>
+    <p>
+        <span class="emp">Measurements</span>. It is important to keep track not only of your exercising results but how it affects your body.<br />
+        Rather than create defined set of measurements/results you have a posibility to define as many as you want -
+        for example if you want to keep track of your blood pressure - just create new measurement called "Blood pressure" - and you are ready to go!
+    </p>
+    <p>
+        <span class="emp">Statistics</span> - numbers are important but sometimes there is a nedd to visualise exercising results or changes in measuremnts over time.
+        Statistics is a tool to do it - select period you want to see and you get a chart to see how it as changing.
+    </p>
+</div>
