e615b3ffcd5368950fb8c6b104a8b63dd8edd329
Leonti Bielski <prishelec@gmail.com>
2010-10-12 05:31:37 +0200

    Added uservoice feedback widget

--- application/views/template.php
+++ application/views/template.php
@@ -147,6 +147,31 @@ $content->multiplicator = $multiplicator;
   })();
 
 </script>
+
+<script type="text/javascript">
+var uservoiceOptions = {
+  /* required */
+  key: 'fitmaestro',
+  host: 'fitmaestro.uservoice.com',
+  forum: '80801',
+  showTab: true,
+  /* optional */
+  alignment: 'right',
+  background_color:'#f00',
+  text_color: 'white',
+  hover_color: '#06C',
+  lang: 'en'
+};
+
+function _loadUserVoice() {
+  var s = document.createElement('script');
+  s.setAttribute('type', 'text/javascript');
+  s.setAttribute('src', ("https:" == document.location.protocol ? "https://" : "http://") + "cdn.uservoice.com/javascripts/widgets/tab.js");
+  document.getElementsByTagName('head')[0].appendChild(s);
+}
+_loadSuper = window.onload;
+window.onload = (typeof window.onload != 'function') ? _loadUserVoice : function() { _loadSuper(); _loadUserVoice(); };
+</script>
 </body>
 </html>
  
